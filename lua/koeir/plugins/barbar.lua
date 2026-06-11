37c90db47849ab4973d59e77e1998ec7b2367141
koeir <kianjohnban@gmail.com>
2026-06-11 11:05:46 +0800

    log: barbar: remapped buffer cycling to <A-j>, <A-k>

--- lua/koeir/plugins/barbar.lua
+++ lua/koeir/plugins/barbar.lua
@@ -19,8 +19,8 @@ return {
 			local opts = { noremap = true, silent = true }
 
 			-- Move to previous/next
-			map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-			map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+			map("n", "<A-j>", "<Cmd>BufferPrevious<CR>", opts)
+			map("n", "<A-k>", "<Cmd>BufferNext<CR>", opts)
 			-- Re-order to previous/next
 			map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
 			map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
